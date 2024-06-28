1d4cc2142803d107faacbdcb31cdfcabae525681
JamieByers <jamiebyers10@icloud.com>
2024-06-28 19:29:31 +0100

    once again edited env

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,9 +1,6 @@
 // supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
-// const PUBLIC_SUPABASE_URL = import.meta.env.SUPABASE_URL;
-// const PUBLIC_SUPABASE_ANON_KEY = import.meta.env.SUPABASE_ANON_KEY;
-const VITE_PUBLIC_SUPABASE_URL = 'https://vvtibvjudvdwarssjjsl.supabase.co';
-const VITE_PUBLIC_SUPABASE_ANON_KEY =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2dGlidmp1ZHZkd2Fyc3NqanNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkzNTQ4MzcsImV4cCI6MjAzNDkzMDgzN30.-oAczYGRHHPIi6HQ3fF6WoxZOAtmz7IEFpOZDoDvG7E';
 
+const VITE_PUBLIC_SUPABASE_URL = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const VITE_PUBLIC_SUPABASE_ANON_KEY = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient(VITE_PUBLIC_SUPABASE_URL, VITE_PUBLIC_SUPABASE_ANON_KEY);
--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,6 @@ export default defineConfig({
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	envPrefix: 'VITE_'
 });
