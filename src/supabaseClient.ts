a5e5fb4ac122aba6ffeec32f93b71fce71a4f3a2
JamieByers <jamiebyers10@icloud.com>
2024-06-28 19:47:36 +0100

    missed a spot

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -4,4 +4,3 @@ export const supabase = createClient(
 	'https://vvtibvjudvdwarssjjsl.supabase.co',
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2dGlidmp1ZHZkd2Fyc3NqanNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkzNTQ4MzcsImV4cCI6MjAzNDkzMDgzN30.-oAczYGRHHPIi6HQ3fF6WoxZOAtmz7IEFpOZDoDvG7E'
 );
-console.log(supabase);
